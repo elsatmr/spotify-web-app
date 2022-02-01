66bb087d11b13c69400c74e1a89c0b98baa08cea
elsatmr <elsaatamara@gmail.com>
2022-01-31 21:28:16 -0800

    Rerouting search to connect to Spotify API

--- src/redux/actions/SearchActions.ts
+++ src/redux/actions/SearchActions.ts
@@ -6,9 +6,10 @@ export const clearSearchItemAction = createAction('CLEAR_SEARCH_ITEM');
 export const getSearchItemAction = createAsyncThunk(
   'SearchReducer/getSearchItemAction',
   async (searchTerm: string) => {
+    const accessToken = window.sessionStorage.getItem('accessToken');
     const res = await axiosServerRequest<any>(
       Methods.GET,
-      `/api/v1/search/${searchTerm}`
+      `/api/v1/search/${searchTerm}/${accessToken}`
     );
     console.log(res.data);
     return res.data;
